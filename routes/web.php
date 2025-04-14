9fa37c4a9fd704e8987db7cf526c44bf30d5a046
chiegremoryy <chiescode@gmail.com>
2025-04-14 17:19:23 +0700

    Implementation RBAC (Role-Based Access Control) - Auth, Role::Pasien, Role::Dokter with Middleware

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,20 @@ Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 
 //routes untuk role pasien
-Route::get('/pasien', [PasienController::class, 'index'])->name('dashboardPasien');
-Route::get('/pasien/periksa', [PasienController::class, 'showPeriksa'])->name('periksaPasien');
-Route::post('/pasien/periksa', [PasienController::class, 'create'])->name('periksaCreate');
+Route::get('/pasien', [PasienController::class, 'index'])->name('dashboardPasien')->middleware('role:pasien', 'auth');
+Route::get('/pasien/periksa', [PasienController::class, 'showPeriksa'])->name('periksaPasien')->middleware('role:pasien', 'auth');
+Route::post('/pasien/periksa', [PasienController::class, 'create'])->name('periksaCreate')->middleware('role:pasien', 'auth');
 
 //routes untuk role dokter
-Route::get('/dokter', [DokterController::class, 'index'])->name('dashboardDokter');
-Route::get('/dokter/memeriksa', [DokterController::class, 'showMemeriksa'])->name('memeriksaDokter');
-Route::get('/dokter/memeriksa/{id}/edit', [DokterController::class, 'editMemeriksa'])->name('memeriksaEdit');
-Route::put('/dokter/memeriksa/{id}/edit', [DokterController::class, 'update'])->name('memeriksaUpdate');
-Route::get('/dokter/obat', [DokterController::class, 'showObat'])->name('obatDokter');
-Route::post('/dokter/obat', [DokterController::class, 'createObat'])->name('obatCreate');
-Route::put('/dokter/obat/{id}/edit', [DokterController::class, 'updateObat'])->name('obatUpdate');
-Route::get('/dokter/obat/{id}/edit', [DokterController::class, 'editObat'])->name('obatEdit');
-Route::delete('/dokter/obat/{id}', [DokterController::class, 'deleteObat'])->name('obatDelete');
+Route::get('/dokter', [DokterController::class, 'index'])->name('dashboardDokter')->middleware('role:dokter', 'auth');
+Route::get('/dokter/memeriksa', [DokterController::class, 'showMemeriksa'])->name('memeriksaDokter')->middleware('role:dokter', 'auth');
+Route::get('/dokter/memeriksa/{id}/edit', [DokterController::class, 'editMemeriksa'])->name('memeriksaEdit')->middleware('role:dokter', 'auth');
+Route::put('/dokter/memeriksa/{id}/edit', [DokterController::class, 'update'])->name('memeriksaUpdate')->middleware('role:dokter', 'auth');
+Route::get('/dokter/obat', [DokterController::class, 'showObat'])->name('obatDokter')->middleware('role:dokter', 'auth');
+Route::post('/dokter/obat', [DokterController::class, 'createObat'])->name('obatCreate')->middleware('role:dokter', 'auth');
+Route::put('/dokter/obat/{id}/edit', [DokterController::class, 'updateObat'])->name('obatUpdate')->middleware('role:dokter', 'auth');
+Route::get('/dokter/obat/{id}/edit', [DokterController::class, 'editObat'])->name('obatEdit')->middleware('role:dokter', 'auth');
+Route::delete('/dokter/obat/{id}', [DokterController::class, 'deleteObat'])->name('obatDelete')->middleware('role:dokter', 'auth');
